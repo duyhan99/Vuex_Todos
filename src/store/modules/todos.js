4f06bed70c13bdcda0faf1ed8ca4949e39de2d41
duyhan99 <duyhan1602@gmail.com>
2021-09-12 17:02:21 +0700

    done

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -35,13 +35,27 @@ const actions = {
       const res = await axios.get(`https://jsonplaceholder.typicode.com/todos?_limit=${limit}`)
 
       commit('setTodos', res.data)
+    },
+
+    async updateTodo({ commit }, updTodo){
+      const res = await axios.put(`https://jsonplaceholder.typicode.com/todos/${updTodo.id}`, updTodo)
+
+      console.log(res)
+
+      commit('updateTodo', res.data)
     }
 };
 
 const mutations = {
   setTodos: (state, todos) => (state.todos = todos),
   newTodo: (state, todo) => (state.todos.unshift(todo)),
-  removeTodo: (state, id) => (state.todos = state.todos.filter(todo => todo.id !== id))
+  removeTodo: (state, id) => (state.todos = state.todos.filter(todo => todo.id !== id)),
+  updateTodo: (state, updTodo) =>{
+    const index = state.todos.findIndex(todo => todo.id === updTodo.id)
+    if(index !== -1){
+      state.todos.splice(index, 1, updTodo)
+    }
+  }
 };
 
 export default {
